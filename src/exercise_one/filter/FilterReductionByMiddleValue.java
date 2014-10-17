fb24cf509a8a0e870d6767f3bf8c809a9c9809b9
xSmorpheusSx <andrebarthel@reimis.net>
2014-10-17 16:27:40 +0200

    removed not yet implemented methods

--- src/exercise_one/filter/FilterReductionByMiddleValue.java
+++ src/exercise_one/filter/FilterReductionByMiddleValue.java
@@ -4,13 +4,11 @@ import java.util.List;
 import java.util.TreeMap;
 
 import exercise_one.Image;
-import exercise_one.converter.StringToInteger;
 import exercise_one.exception.ImageException;
+import exercise_one.exception.NotYetImplementedException;
 import exercise_one.model.color.Colormodel;
 import exercise_one.model.color.RGB;
 import exercise_one.model.matrix.Coordinate;
-import exercise_one.validator.ValidatorPixelCount;
-import exercise_one.validator.ValidatorRowCount;
 
 public class FilterReductionByMiddleValue extends Filter<RGB> {
 	
@@ -28,49 +26,30 @@ public class FilterReductionByMiddleValue extends Filter<RGB> {
 	
 	@Override
 	public RGB filter(List<Character> lstCorrdinate) {
-		this.setValidRow();
-		this.setValidCol();
-		int value = StringToInteger.convert(lstCorrdinate);
-     	switch(super.getCurrentChannel()){
-        	case RED:  	super.setGreenChannel();
-        				this.average_red += value;
-        				break;
-        	case GREEN: super.setBlueChannel();
-        				this.average_green += value;
-        				break;
-        	case BLUE:	super.setRedChannel();
-        				this.average_blue += value;
-        				break;
-     	}
-		if(this.validCol){
-             super.getCurrentPixel().setRed(this.average_red/3);
-             super.getCurrentPixel().setGreen(this.average_green/3);
-        	 super.getCurrentPixel().setBlue(this.average_blue/3);
-			 return super.getCurrentPixel();
+		try{
+			throw new NotYetImplementedException();
+		}catch(NotYetImplementedException e){
+			e.printStackTrace();
 		}
-//		super.getDimension().increaseColumn();
 		return null;
 	}
 	
-	private void setValidRow(){
-		this.validRow = super.getDimension().getRow() != 0 || (super.getDimension().getRow()%OFFSET_ROW) == 0;
-	}
-	
-	private void setValidCol(){
-		this.validCol = super.getDimension().getColumn() != 0 || (super.getDimension().getColumn()%OFFSET_COL) == 0;
-	}
-
 	@Override
 	public void reset() {
-		this.average_red = 0;
-		this.average_green = 0;
-		this.average_blue = 0;
+		try{
+			throw new NotYetImplementedException();
+		}catch(NotYetImplementedException e){
+			e.printStackTrace();
+		}
 	}
 
 	@Override
 	public void validate(Image image) throws ImageException {
-		new ValidatorPixelCount().validate(super.getDimension(), image);
-		new ValidatorRowCount().validate(super.getDimension());
+		try{
+			throw new NotYetImplementedException();
+		}catch(NotYetImplementedException e){
+			e.printStackTrace();
+		}
 	}
 
 	
