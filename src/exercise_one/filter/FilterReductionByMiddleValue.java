300cd3b66fd5304b683af470f150e3999f8c133c
xSmorpheusSx <andrebarthel@reimis.net>
2014-10-18 11:09:37 +0200

    removed filter calculation by middle value

--- src/exercise_one/filter/FilterReductionByMiddleValue.java
+++ src/exercise_one/filter/FilterReductionByMiddleValue.java
@@ -4,12 +4,10 @@ import java.util.List;
 import java.util.TreeMap;
 
 import exercise_one.Image;
-import exercise_one.converter.ConverterStringToInteger;
 import exercise_one.exception.ImageException;
+import exercise_one.exception.NotYetImplementedException;
 import exercise_one.model.color.RGB;
 import exercise_one.model.matrix.Coordinate;
-import exercise_one.validator.ValidatorPixelCount;
-import exercise_one.validator.ValidatorRowCount;
 
 public class FilterReductionByMiddleValue extends Filter<RGB> {
 	
@@ -19,61 +17,24 @@ public class FilterReductionByMiddleValue extends Filter<RGB> {
 	
 	@Override
 	public TreeMap<Coordinate, RGB> filter(List<Character> lstCorrdinate) {
-		Coordinate startingPointCoordinate = this.getStartingPoint();
-		int value = ConverterStringToInteger.convert(lstCorrdinate);
-		if(!sum_rgb.containsKey(startingPointCoordinate)){
-			sum_rgb.put(startingPointCoordinate, new RGB());
-		}
-     	switch(super.getCurrentChannel()){
-        	case RED:  	super.setGreenChannel();
-        				this.sum_rgb.get(startingPointCoordinate).increaseRed(value/OFFSET);
-        				break;
-        	case GREEN: super.setBlueChannel();
-        				this.sum_rgb.get(startingPointCoordinate).increaseGreen(value/OFFSET);
-        				break;
-        	case BLUE:	super.setRedChannel();
-        				this.sum_rgb.get(startingPointCoordinate).increaseBlue(value/OFFSET);
-        				break;
-     	}
-		if(this.isValidRow()){
-			if(this.isValidColumn()){
-				return sum_rgb;
-			}
+		try{
+			throw new NotYetImplementedException();
+		}catch(NotYetImplementedException e){
+			e.printStackTrace();
 		}
 		return null;
 	}
-	
-	private Coordinate getStartingPoint(){
-		int x = super.getDimension().getColumn()-OFFSET+(super.getDimension().getColumn()%OFFSET);
-		int y = super.getDimension().getRow()-OFFSET+(super.getDimension().getRow()%OFFSET);
-		if(x < 0){
-			x = 0;
-		}
-		if(y < 0){
-			y = 0;
-		}
-		return new Coordinate(x, y);
-	}
-	
-	private boolean isValidRow(){
-		return super.getDimension().getRow() != 0 && (super.getDimension().getRow()%OFFSET) == 0;
-	}
-	
-	private boolean isValidColumn(){
-		return super.getDimension().getColumn() != 0 && (super.getDimension().getColumn()%OFFSET) == 0;
-	}
-	
+
 	@Override
 	public void reset() {
+		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void validate(Image image) throws ImageException {
-		new ValidatorPixelCount().validate(super.getDimension(), image);
-		new ValidatorRowCount().validate(super.getDimension());
+		// TODO Auto-generated method stub
+		
 	}
-
-	
 	
 }
