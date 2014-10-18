1ca8e0aec8c2a40b354a21ae37f9fa1915189162
xSmorpheusSx <andrebarthel@reimis.net>
2014-10-18 14:20:48 +0200

    default filter eingestellt

--- src/exercise_one/JpegEncoder.java
+++ src/exercise_one/JpegEncoder.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import exercise_one.exception.ImageException;
 import exercise_one.exception.UnsupportedImageFormatException;
 import exercise_one.filter.Filter;
-import exercise_one.filter.FilterReductionByMiddleValue;
+import exercise_one.filter.FilterDefault;
 import exercise_one.logger.CoordinateLogger;
 
 public class JpegEncoder
@@ -15,8 +15,8 @@ public class JpegEncoder
         try
         {   
         	long start = System.currentTimeMillis();
-        	Filter filter = new FilterReductionByMiddleValue();
-            Image source = new Image("C:\\Users\\xSmorpheusSx\\Desktop\\1257989_1_DSC_0237-2.ppm", filter);
+        	Filter filter = new FilterDefault();
+            Image source = new Image("C:\\Users\\xSmorpheusSx\\Desktop\\ppm_bild.ppm", filter);
             new CoordinateLogger(source.getPixels(), filter.getDimension().getWidth(), filter.getDimension().getHeight()).log(true);
             //System.out.println(System.currentTimeMillis() - start);
         }
